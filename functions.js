6e3c791e6baeb8f141e99657bf5ba659b114d0af
benbeatty85 <benbeatty85@gmail.com>
2018-06-27 17:09:04 -0400

    changed headline animation

--- functions.js
+++ functions.js
@@ -70,13 +70,27 @@
         bodyClassList.remove('nav-open');
     })
 
+//Animated Headline on home page
 
-    //Floating in headline
+// Wrap every letter in a span
 
+$('.ml6 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
 
-    $(document).ready(function() {
-        $(window).bind("load", function () {
-            $('.fly-in-text').removeClass('hidden');
-        })
-    }) 
-
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: function(el, i) {
+      return 50 * i;
+    }
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 100000
+  });
